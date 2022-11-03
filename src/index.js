045d1bcdc98793e7f0bcb0bea7fee75d7ac92e63
paul_lestyo <paulus.lestyo@student.uns.ac.id>
2022-11-03 20:06:41 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ContextProvider } from './Context/Context';
-import { BrowserRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <ContextProvider>
       <App />
     </ContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
